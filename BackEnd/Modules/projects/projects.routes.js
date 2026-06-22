c74896356ce446297616b6321e257d8632c105ff
Lubna <Lubna.jaradat14@gmail.com>
2026-06-22 18:26:36 +0300

    feat: add routes for listing, creating and updating EPs

---
+++ BackEnd/Modules/projects/projects.routes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware = require("../Middlewares/auth.middleware");
+const isAdmin = require("../Middlewares/isAdmin.middleware");
+const validate = require("../Middlewares/validation");
+const projectController = require("./projects.controller");
+const createProjectSchema = require("./schema/createProject.schema");
+const {
+  updateProjectSchema,
+  updateProjectParamSchema,
+} = require("./schema/updateProject.schema");
+const { paginationSchema } = require("../utils/schema.utils");
+
+router.use(authMiddleware);
+
+router.get("", validate({ query: paginationSchema }), projectController.listProjects);
+
+router.post(
+  "/",
+  isAdmin,
+  validate({ body: createProjectSchema }),
+  projectController.createProject,
+);
+
+router.patch(
+  "/:id",
+  isAdmin,
+  validate({ params: updateProjectParamSchema, body: updateProjectSchema }),
+  projectController.updateProject,
+);
+
+module.exports = router;
